d48098ddfa3ab0be5a8b3655d3f798ecc9860e7e
Cali Tabilas <ctabilas23@gmail.com>
2019-05-22 17:23:03 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log(`1. Traversing through departments`)
+
+let company = { // the same object, compressed for brevity
+  sales: [
+    {name: 'John', salary: 1000}, 
+    {name: 'Alice', salary: 600 }
+  ],
+  development: {
+    sites: [
+      {name: 'Peter', salary: 2000}, 
+      {name: 'Alex', salary: 1800 }
+    ],
+    internals: [
+      {name: 'Jack', salary: 1300}
+    ]
+  }
+};
+
+
